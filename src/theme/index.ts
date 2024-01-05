dbf5f2de0a948c3e643936abbee2213cfcc9d0db
Gui Monteiro <guilhermemonteiroro@gmail.com>
2024-01-05 10:25:30 -0300

    Config styles and themeProvider

---
+++ src/theme/index.ts
@@ -0,0 +1,30 @@
+export default {
+    COLORS: {
+        RED_LIGHT: '#F4E6E7',
+        RED_MID: '#F3BABD',
+        RED_DARK: '#BF3B44',
+        GREEN_LIGHT: '#E5F0DB',
+        GREEN_MID: '#CBE4B4',
+        GREEN_DARK: '#639339',
+        GRAY_7: '#FAFAFA',
+        GRAY_6: '#EFF0F0',
+        GRAY_5: '#DDDEDF',
+        GRAY_4: '#B9BBBC',
+        GRAY_3: '#5C6265',
+        GRAY_2: '#333638',
+        GRAY_1: '#1B1D1E',
+        WHITE: '#FFFFFF'
+    },
+    FONT_FAMILY: {
+        REGULAR: '',
+        BOLD: ''
+    },
+    FONT_SIZE: {
+        SM: 12,
+        SM_2: 14,
+        MD: 16,
+        MD_2: 18,
+        LG: 24,
+        XL: 32
+    }
+}
